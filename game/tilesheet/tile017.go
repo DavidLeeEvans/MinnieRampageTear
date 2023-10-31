f9b546897efe04d18e1c3d00d74c0d2094d6fa16
David Lee Evans <dle.evans@gmail.com>
2023-10-31 09:39:05 -0400

    wip

--- game/tilesheet/tile017.go
+++ game/tilesheet/tile017.go
@@ -2,7 +2,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/game/tilesheet/tilesheet.tilesource\"\n"
-  "default_animation: \"tile001\"\n"
+  "default_animation: \"tile017\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
