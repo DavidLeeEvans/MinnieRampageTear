04beb49276d96d1ffc166c1d9ffa9d176458b8e1
David Lee Evans <dle.evans@gmail.com>
2023-11-04 15:04:20 -0400

    wip

--- fb_production_build.sh
+++ fb_production_build.sh
@@ -3,8 +3,7 @@ rm -f ./prodution_app.zip
 rm -fr ./bundle
 mkdir -p ./bundle/HTML5
 # Bundle for HTML5 with architectures: js-web,wasm-web
-./run_build.sh && /home/david/bin/Defold/packages/jdk-17.0.5+8/bin/java -cp "/home/david/bin/Defold/packages/defold-7e5c022ee89d625257f3e22797f49b51d60ba2ef.jar" com.dynamo.bob.Bob --archive --platform "js-web" --architectures "js-web,wasm-web" --bundle-output "./bundle/HTML5" --build-report-html "./bundle/HTML5/build-report.html" --email "dle.evans@gmail.com" --texture-compression true --variant release --with-symbols resolve distclean build bundle &&
-#zip -r prodution_app.zip  /home/david/DEFOLD/8/Rili/bundle/HTML5/RailRoad\ Runner/*
+./run_build.sh && /home/david/bin/Defold/packages/jdk-17.0.5+8/bin/java -cp "/home/david/bin/Defold/packages/defold-b364f346962cf24b6845a52c0c97beb5deb7a8f1.jar" com.dynamo.bob.Bob --archive --platform "js-web" --architectures "js-web,wasm-web" --bundle-output "./bundle/HTML5" --build-report-html "./bundle/HTML5/build-report.html" --email "dle.evans@gmail.com" --texture-compression true --variant release --with-symbols resolve distclean build bundle
 
-#cd  /home/david/DEFOLD/8/Rili/bundle/HTML5/RailRoad\ Runner/
-#zip -r /home/david/DEFOLD/8/Rili/prodution_app.zip  *
+cd  /home/david/DEFOLD/MinnietaurGame/bundle/HTML5/Minnietaur
+zip -r /home/david/DEFOLD/MinnietaurGame/prodution_app.zip  *
