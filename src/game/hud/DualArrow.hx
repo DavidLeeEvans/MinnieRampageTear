f635322c23882952cd136e6020ef250e685f8e2e
David Lee Evans <dle.evans@gmail.com>
2023-11-07 17:07:43 -0500

    wip

--- src/game/hud/DualArrow.hx
+++ src/game/hud/DualArrow.hx
@@ -1,7 +1,8 @@
 package game.hud;
+
+import Defold.hash;
 import defold.Go.GoMessages;
 import defold.Msg;
-import Defold.hash;
 import defold.support.Script;
 import defold.support.ScriptOnInputAction;
 import defold.types.Hash;
@@ -10,15 +11,23 @@ import defold.types.Url;
 
 private typedef DualArrowData = {}
 
+@:build(defold.support.MessageBuilder.build()) class DualArrowMessage {
+	var press_up_down:{result:Bool};
+	var on_off:{data:Bool};
+}
+
 class DualArrow extends Script<DualArrowData> {
 	override function init(self:DualArrowData) {
-	Msg.post(".", GoMessages.acquire_input_focus);
-}
+		Msg.post(".", GoMessages.acquire_input_focus);
+	}
 
 	override function update(self:DualArrowData, dt:Float):Void {}
 
 	override function on_message<T>(self:DualArrowData, message_id:Message<T>, message:T, sender:Url):Void {
-		switch (message_id) {}
+		switch (message_id) {
+			case DualArrowMessage.press_up_down:
+			case DualArrowMessage.on_off:
+		}
 	}
 
 	override function on_input(self:DualArrowData, action_id:Hash, action:ScriptOnInputAction):Bool {
