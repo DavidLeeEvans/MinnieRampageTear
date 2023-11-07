2c52eecdd2f64fc1681804341bed147fe7c967f3
David Lee Evans <dle.evans@gmail.com>
2023-11-07 16:44:53 -0500

    wip

---
+++ hud/dual_arrow.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/hud/dual_arrow.atlas\"\n"
+  "default_animation: \"prompt\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
