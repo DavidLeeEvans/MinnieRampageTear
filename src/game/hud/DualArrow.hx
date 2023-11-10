9e3d91f7dac0cda6c410e1d4a0d2655e637be8bf
David Lee Evans <dle.evans@gmail.com>
2023-11-10 15:31:44 -0500

    wip

--- src/game/hud/DualArrow.hx
+++ src/game/hud/DualArrow.hx
@@ -1,16 +1,10 @@
 package game.hud;
 
-import Defold.hash;
 import defold.Go;
 import defold.Msg;
-import defold.Vmath;
 import defold.support.Script;
-import defold.support.ScriptOnInputAction;
 import defold.types.Hash;
 import defold.types.Message;
-import defold.types.Url;
-import defold.types.Vector3;
-import dex.lib.orthographic.Camera;
 
 @:build(defold.support.HashBuilder.build()) class DualArrowHash {
 	var i0;
@@ -23,14 +17,7 @@ import dex.lib.orthographic.Camera;
 	var prompt;
 }
 
-private typedef DualArrowData = {
-	var start:Vector3;
-	var end:Vector3;
-	@property(11.1) var length:Float;
-	@property var target:Hash;
-	@property var top_pt:Vector3;
-	@property var bottom_pt:Vector3;
-}
+private typedef DualArrowData = {}
 
 @:build(defold.support.MessageBuilder.build()) class DualArrowMessage {
 	var press_up_down:{result:Bool};
@@ -78,43 +65,7 @@ class DualArrow extends Script<DualArrowData> {
 		// Msg.post("@render:", RenderMessages.draw_line, l3);
 	}
 
-	override function on_message<T>(self:DualArrowData, message_id:Message<T>, message:T, sender:Url):Void {
-		switch (message_id) {
-			case DualArrowMessage.press_up_down:
-			// if(message.result)
-			case DualArrowMessage.on_off:
-				if (message.data) {
-					Msg.post(".", GoMessages.acquire_input_focus);
-					Msg.post(DualArrowHash.i0, GoMessages.disable);
-				} else {
-					Msg.post(".", GoMessages.release_input_focus);
-				}
-		}
-	}
-
-	override function on_input(self:DualArrowData, action_id:Hash, action:ScriptOnInputAction):Bool {
-		if (action_id == hash("touch")) {
-			final bounding_box = Go.get_world_position();
-			Defold.pprint(bounding_box);
-			Defold.pprint('screenx ${action.screen_x} screeny ${action.screen_y}');
-			Defold.pprint('touch x ${action.x} touch y ${action.y}');
-			Defold.pprint(' the action_id is $action_id');
-			Defold.pprint(' the action is $action');
-			Defold.pprint('----------- World Screen -----------------');
-			Defold.pprint(Camera.screen_to_world(hash("/camera"), Vmath.vector3(action.screen_x, action.screen_y, 0)));
-			if (action.pressed) {
-				trace('================================ Dual Arrow Pressed========================================');
-			} else if (action.released) {
-				trace('=============================== Dual Arrow Released =======================================');
-			}
-		}
-
-		return true;
-	}
-
-	override function final_(self:DualArrowData):Void {
-		Msg.post(".", GoMessages.release_input_focus);
-	}
+	override function final_(self:DualArrowData):Void {}
 
 	override function on_reload(self:DualArrowData):Void {}
 }
--- src/game/hud/Touch.hx
+++ src/game/hud/Touch.hx
@@ -1,38 +1,69 @@
 package game.hud;
-import defold.Go.GoMessages;
-import defold.Msg;
+
 import Defold.hash;
+import defold.Go;
+import defold.Msg;
+import defold.Physics.PhysicsMessages;
+import defold.Vmath;
 import defold.support.Script;
 import defold.support.ScriptOnInputAction;
 import defold.types.Hash;
 import defold.types.Message;
 import defold.types.Url;
+import defold.types.Vector3;
+import dex.lib.orthographic.Camera;
 
-private typedef TouchData = {}
+private typedef TouchData = {
+	@property var target:Hash;
+	@property(11.1) var length:Float;
+	@property var top_pt:Vector3;
+	@property var bottom_pt:Vector3;
+	var _working:Bool;
+}
 
 class Touch extends Script<TouchData> {
 	override function init(self:TouchData) {
-	Msg.post(".", GoMessages.acquire_input_focus);
-}
+		Msg.post(".", GoMessages.acquire_input_focus);
+		Go.set_position(Vmath.vector3(-100, -100, 0));
+		self._working = false;
+	}
 
-	override function update(self:TouchData, dt:Float):Void {}
+	override function update(self:TouchData, dt:Float):Void {
+		if (self._working) {
+			Defold.pprint("------ update ------");
+			Defold.pprint(Go.get_world_position());
+			Go.set_position(Go.get_world_position(), self.target);
+		}
+	}
 
 	override function on_message<T>(self:TouchData, message_id:Message<T>, message:T, sender:Url):Void {
-		switch (message_id) {}
+		switch (message_id) {
+			case PhysicsMessages.collision_response:
+				Defold.pprint("----- Touch Collision -----");
+				self._working = true;
+		}
 	}
 
 	override function on_input(self:TouchData, action_id:Hash, action:ScriptOnInputAction):Bool {
-		if (action_id == hash("jump") || action_id == hash("touch")) {
-			if (action.pressed)
-				trace('jump(data)');
-			else if (action.released)
-				trace('abort_jump(data)');
+		if (action_id == hash("touch")) {
+			if (action.pressed) {
+				trace('Touch Press');
+				final _v = Vmath.vector3(action.x, action.y, 0);
+				final _pos = Camera.screen_to_world(hash("/camera"), _v);
+				Go.set_position(_pos, ".");
+			} else if (action.released) {
+				trace('Touch Released');
+				Go.set_position(Vmath.vector3(-100, -100, 0));
+				self._working = false;
+			}
 		}
 
 		return false;
 	}
 
-	override function final_(self:TouchData):Void {}
+	override function final_(self:TouchData):Void {
+		Msg.post(".", GoMessages.release_input_focus);
+	}
 
 	override function on_reload(self:TouchData):Void {}
 }
