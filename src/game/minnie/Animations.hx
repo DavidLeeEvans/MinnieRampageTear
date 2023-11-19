c61648dabcfb785d53d08eeb51853a6526cbcd15
David Lee Evans <dle.evans@gmail.com>
2023-11-19 17:33:41 -0500

    wip

---
+++ src/game/minnie/Animations.hx
@@ -0,0 +1,26 @@
+package game.minnie;
+import defold.Go.GoMessages;
+import defold.Msg;
+import Defold.hash;
+import defold.support.Script;
+import defold.support.ScriptOnInputAction;
+import defold.types.Hash;
+import defold.types.Message;
+import defold.types.Url;
+
+private typedef AnimationsData = {}
+
+class Animations extends Script<AnimationsData> {
+	override function init(self:AnimationsData) {
+}
+
+	override function update(self:AnimationsData, dt:Float):Void {}
+
+	override function on_message<T>(self:AnimationsData, message_id:Message<T>, message:T, sender:Url):Void {
+		switch (message_id) {}
+	}
+
+	override function final_(self:AnimationsData):Void {}
+
+	override function on_reload(self:AnimationsData):Void {}
+}
---
+++ src/game/minnie/Physics.hx
@@ -0,0 +1,25 @@
+package game.minnie;
+import defold.Go.GoMessages;
+import defold.Msg;
+import Defold.hash;
+import defold.support.Script;
+import defold.support.ScriptOnInputAction;
+import defold.types.Hash;
+import defold.types.Message;
+import defold.types.Url;
+
+private typedef PhysicsData = {}
+
+class Physics extends Script<PhysicsData> {
+}
+
+	override function update(self:PhysicsData, dt:Float):Void {}
+
+	override function on_message<T>(self:PhysicsData, message_id:Message<T>, message:T, sender:Url):Void {
+		switch (message_id) {}
+	}
+
+	override function final_(self:PhysicsData):Void {}
+
+	override function on_reload(self:PhysicsData):Void {}
+}
