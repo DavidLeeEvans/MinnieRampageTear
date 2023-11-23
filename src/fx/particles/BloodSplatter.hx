14e287fc649d0320680eefad23c00fe91cf24ff1
David Lee Evans <dle.evans@gmail.com>
2023-11-23 12:34:01 -0500

    wip

--- src/fx/particles/BloodSplatter.hx
+++ src/fx/particles/BloodSplatter.hx
@@ -1,7 +1,9 @@
 package fx.particles;
+
+import Defold.hash;
 import defold.Go.GoMessages;
 import defold.Msg;
-import Defold.hash;
+import defold.Particlefx;
 import defold.support.Script;
 import defold.support.ScriptOnInputAction;
 import defold.types.Hash;
@@ -12,9 +14,15 @@ private typedef BloodSplatterData = {}
 
 class BloodSplatter extends Script<BloodSplatterData> {
 	override function init(self:BloodSplatterData) {
-}
+		// TODO testing start the effect component "particles" in the current game object
+		Particlefx.play("#particles");
+		// TODO testing stop the effect component "particles" in the current game object
+		final _pso:ParticleFxStopOptions = {clear: true};
+		Particlefx.stop("#particles", _pso);
+	}
 
 	override function update(self:BloodSplatterData, dt:Float):Void {}
+
 	override function on_message<T>(self:BloodSplatterData, message_id:Message<T>, message:T, sender:Url):Void {
 		switch (message_id) {}
 	}
